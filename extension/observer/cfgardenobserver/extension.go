b6130d54de03a5dda70fee39bd6c4a102b8562ad
Tomás Mota <tomas.mota@springernature.com>
2024-07-31 15:45:44 +0100

    use tags as labels

--- extension/observer/cfgardenobserver/extension.go
+++ extension/observer/cfgardenobserver/extension.go
@@ -5,8 +5,8 @@ package cfgardenobserver // import "github.com/open-telemetry/opentelemetry-coll
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
-	"strings"
 	"time"
 
 	"code.cloudfoundry.org/garden"
@@ -87,13 +87,13 @@ func (g *cfGardenObserver) containerEndpoint(c garden.Container) *observer.Endpo
 
 	handle := c.Handle()
 	details := &observer.Container{
-		Name:          handle, //TODO use application name / container id
+		Name:          handle, // TODO use application name / container id
 		ContainerID:   handle,
 		Host:          info.ContainerIP,
 		Port:          8080,
 		AlternatePort: 61001,
 		Transport:     observer.ProtocolTCP,
-		Labels:        containerLabels(info),
+		Labels:        g.containerLabels(info),
 	}
 
 	endpoint := observer.Endpoint{
@@ -105,12 +105,63 @@ func (g *cfGardenObserver) containerEndpoint(c garden.Container) *observer.Endpo
 	return &endpoint
 }
 
-func containerLabels(info garden.ContainerInfo) map[string]string {
-	labels := make(map[string]string)
-	for k, v := range info.Properties {
-		if strings.HasPrefix(k, "network") {
-			labels[k] = v
+func (g *cfGardenObserver) containerLabels(info garden.ContainerInfo) map[string]string {
+	tags, err := parseTags(info)
+	if err != nil {
+		g.logger.Warn("not able to parse container tags into labels", zap.Error(err))
+		return nil
+	}
+
+	return tags
+}
+
+// The info looks like this:
+//
+//		{
+//		  "log_config": {
+//		    "guid": "aaaaaaaa-bbbb-cccc-dddd-eeeeeeeeeeee",
+//		    "index": 0,
+//		    "source_name": "CELL",
+//		    "tags": {
+//		      "app_id": "aaaaaaaa-bbbb-cccc-dddd-eeeeeeeeeeee",
+//		      "app_name": "example-app",
+//		      "instance_id": "0",
+//		      "organization_id": "11111111-2222-3333-4444-555555555555",
+//		      "organization_name": "example-org",
+//		      "process_id": "aaaaaaaa-bbbb-cccc-dddd-eeeeeeeeeeee",
+//		      "process_instance_id": "abcdef12-3456-7890-abcd-ef1234567890",
+//		      "process_type": "web",
+//		      "source_id": "aaaaaaaa-bbbb-cccc-dddd-eeeeeeeeeeee",
+//		      "space_id": "99999999-8888-7777-6666-555555555555",
+//		      "space_name": "example-space"
+//		    }
+//		  }
+//		}
+//
+//	 We parse only the tags into a map, to be used as labels
+func parseTags(info garden.ContainerInfo) (map[string]string, error) {
+	logConfig, ok := info.Properties["log_config"]
+	if !ok {
+		return nil, fmt.Errorf("container properties do not have log_config field")
+	}
+
+	var data map[string]any
+	err := json.Unmarshal([]byte(logConfig), &data)
+	if err != nil {
+		return nil, fmt.Errorf("error unmarshaling logConfig: %v", err)
+	}
+
+	tags, ok := data["tags"].(map[string]any)
+	if !ok {
+		return nil, fmt.Errorf("expected tags field to be a map. got=%T", data["tags"])
+	}
+
+	result := make(map[string]string)
+	for key, value := range tags {
+		if strValue, ok := value.(string); ok {
+			result[key] = strValue
 		}
 	}
-	return labels
+
+	return result, nil
 }
